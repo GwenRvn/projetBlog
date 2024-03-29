26ac47383cfb2fb715f2e7ceddd1bccab4273218
Gwendoline Raveino <naheirvn@gmail.com>
2024-03-28 15:10:50 -1000

    Projet Blog

---
+++ templates/error_404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Request</title>
+</head>
+<body>
+<h1>404 Bad Request</h1>
+<p>Sorry, your request cannot be fulfilled due to bad syntax or invalid request.</p>
+</body>
+</html>
---
+++ templates/single.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+</head>
+<body>
+<div>
+    <h1>Mon blog</h1>
+    <p>En construction</p>
+    <div>
+        <h2><?= htmlspecialchars($article->getTitle());?></h2>
+        <p><?= htmlspecialchars($article->getContent());?></p>
+        <p><?= htmlspecialchars($article->getAuthor());?></p>
+        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
+    </div>
+    <br>
+    <a href="../public/index.php">Retour à l'accueil</a>
+    <div id="comments" class="text-left" style="margin-left: 50px">
+        <h3>Commentaires</h3>
+        <?php
+        foreach ($comments as $comment)
+        {
+            ?>
+            <h4><?= htmlspecialchars($comment->getPseudo());?></h4>
+            <p><?= htmlspecialchars($comment->getContent());?></p>
+            <p>Posté le <?= htmlspecialchars($comment->getCreatedAt());?></p>
+            <?php
+        }
+        ?>
+    </div>
+    <form action="index.php?route=addComment&articleId=<?= $article->getId();?>" method="post">
+        <label for="pseudo">Nom :</label><br>
+        <input type="text" id="pseudo" name="pseudo" required><br><br>
+
+        <label for="commentaire">Commentaire :</label><br>
+        <textarea id="content" name="content" rows="4" cols="50" required></textarea><br><br>
+
+        <input type="submit" value="Envoyer">
+    </form>
+</div>
+</body>
+</html>
